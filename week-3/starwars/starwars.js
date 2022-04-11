7ddf6cf0618c1e6c65c2c9ef0f19ff12091aac95
jayschreffler <jeffrey.schreffler@bryanuniversity.edu>
2022-04-11 12:24:56 -0400

    added week 3 starwars

---
+++ week-3/starwars/starwars.js
@@ -0,0 +1,46 @@
+// declare any necessary variables
+
+let queryType;
+let itemID;
+
+// define a function called 'fetchData()' that passes the values from 
+// the 'queryType' and 'itemID' elements in starwars.html to the function 
+// called 'getFromSWAPI()'
+
+
+const fetchData = () => {
+     queryType = document.querySelector('#queryType').value;
+     itemID = document.querySelector('#itemID').value;
+     getFromSWAPI(queryType,itemID);
+}
+
+
+function getFromSWAPI(queryType,itemID) {
+    // assign values to any necessary variables
+    fetch(`https://swapi.dev/api/${queryType}/${itemID}`)
+    .then(function (response) {
+        return response.json()
+    })
+    .then(function(data){
+        updateInfo(data)  
+    })
+    .catch(function(err) {
+        console.warn(err)
+    })
+};
+
+const updateInfo = responseJSON => {
+    const keys = Object.keys(responseJSON);
+    document.querySelector("#dataLabel1").innerText = keys[0].replace('/_/g', '' );
+    document.querySelector("#dataValue1").innerText = responseJSON[keys[0]];
+    document.querySelector("#dataLabel2").innerText = keys[5].replace('/_/g', " ");
+    document.querySelector("#dataValue2").innerText = responseJSON[keys[5]];
+}
+
+// getFromSWAPI()
+// create a new function called 'updateInfo()' that receives the data from 
+// the call to that function (see above). Use logic to write the appropriate
+//labels to 'dataLabel1' and 'dataLabel2' elements in starwars.html, as well
+// as the appropriate values from the data object to the 'dataValue1' and 
+// 'dataValue2' elements in starwars.html.
+
