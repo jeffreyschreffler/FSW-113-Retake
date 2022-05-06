a1b2d16dbe8cc83bab0821a6a48a1bcbab3ec17a
jayschreffler <jeffrey.schreffler@bryanuniversity.edu>
2022-05-06 10:00:30 -0400

    added week 6

---
+++ week-6/assignment/scripts/weather.js
@@ -0,0 +1,58 @@
+// import the convertTemp.js and getDaylight.js scripts with their default export
+
+import { blueBlack } from "./getDayight.js";
+
+import { localTemp } from "./convertTemp.js";
+
+// declare any variables needed
+const temp = document.querySelector("#tempData")
+const humidity = document.querySelector("#humidData")
+const conditions = document.querySelector("#conditionsData")
+
+// create an event listener for the click of the goBttn that calls a function to fetch the weather data
+
+document.querySelector('#goBttn').addEventListener('click', () => {
+    getWeatherData(document.getElementById('city').value);
+});
+
+// create a function that calls the function that queries the api 
+// and then creates promises that will call a function to write the weather data to the HTML page        
+
+function getWeatherData(city) {
+    fetchData(city)
+        .then(function(data) {
+            // console.log(data)
+            toLocal(data);
+        })
+        .catch(function(err) {
+            console.warn(err);
+        })
+}
+
+
+// use an asynchronous call to fetches the current weather for the specified city, awaits it, 
+// and returns the resulting data
+
+const fetchData = async(city) => {
+    const response = await fetch (`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=cb88ccd906064cfe6a757fee8c8dce55`);
+    try {
+        return  await response.json();
+    } catch (error){
+        console.log(error)
+    }
+}
+
+// create a function that writes the temperature (using local units), humidity, and conditions
+// this function should also change the background color of the weather app to blue during the daylight
+// hours at the specified city
+
+function toLocal(data){
+    temp.innerText = localTemp(data.main.temp, data.sys.country );
+    humidity.innerText = `${data.main.humidity}%`;
+    conditions.innerText = data.weather[0].main;
+    // console.log(data)
+    document.querySelector(".weatherWrapper").style.backgroundColor = blueBlack(data);
+}
+
+//key cb88ccd906064cfe6a757fee8c8dce55
+
